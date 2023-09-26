b8f3c9b36a200289cf37cf96676875c203edf0e3
Norman Aberin <aberinnj@gmail.com>
2023-09-25 20:39:23 -0500

    Added hardLink sync to cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,8 @@
 IDENTIFICATION=$(python get.py "$BRANCH_REF")
 NAMESPACE="ft-$IDENTIFICATION"
 
+# patch hardLink set to true
+kubectl patch --type=merge adb "adb-$IDENTIFICATION" -p '{"spec": {"hardLink": true}}' -n "$NAMESPACE"
+
 # delete
 kubectl delete namespace "$NAMESPACE"
