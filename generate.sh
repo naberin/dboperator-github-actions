afe528ed3834c9acb7fc8642442ad6ad073b3916
aberinnj <aberinnj@gmail.com>
2023-10-10 15:56:52 -0500

    Updated create.yml

--- generate.sh
+++ generate.sh
@@ -2,23 +2,12 @@
 
 # set token
 IDENTIFICATION=$(python get.py "$BRANCH_REF")
-NAMESPACE="ft-$IDENTIFICATION"
-PATCH_LOC="${RUNNER_TEMP}/patch.yaml"
 DBNAME=$(python clean_dbname.py "$IDENTIFICATION")
-
-#  create namespace
-kubectl create namespace "$NAMESPACE"
-
-# create secret
-kubectl create secret -n "$NAMESPACE" generic default-admin-password --from-literal=default-admin-password="$OP_DEF_PWD"
 
 # kustomize remove if exists
 if test -f "kustomization.yaml"; then
   rm kustomization.yaml
 fi
-
-# kustomize create
-kustomize create --resources base
 
 # create patch
 cat <<EOF > patch.yaml
@@ -32,14 +21,3 @@ cat <<EOF > patch.yaml
   path: "/spec/details/displayName"
   value: dbtest-${IDENTIFICATION}
 EOF
-
-# replacing the image name in the k8s template
-kustomize edit set namesuffix "$IDENTIFICATION"
-kustomize edit add patch --path patch.yaml --kind AutonomousDatabase
-kustomize edit set namespace "$NAMESPACE"
-
-# kubectl apply
-kustomize build . | kubectl apply -f -
-
-# move
-mv patch.yaml "$PATCH_LOC"
