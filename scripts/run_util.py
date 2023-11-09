74714a23b7fa2bb49da91872caae155fc9766129
aberinnj <aberinnj@gmail.com>
2023-11-09 17:14:42 -0600

    Updated matrix

--- scripts/run_util.py
+++ scripts/run_util.py
@@ -9,7 +9,7 @@ import json
 def list_schemas(location):
     d = [ x.removeprefix(f'{location}/') for x in [x[0] for x in os.walk(location) if x[0] != location ] ]
     d.remove("admin")
-    return json.dumps(d)
+    return json.dumps({"schemas": d})
     
 
 def clean(with_name):
